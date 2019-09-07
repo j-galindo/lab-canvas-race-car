d5a15b8f1c3aa890c9dcbd02c990d979d1f1d7c7
Jose Galindo <jgalindo.ironhack@gamil.com>
2019-09-07 10:53:52 -0400

    set up canvas

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,23 @@
+function draw() {
+    let canvas = document.getElementById('racecar');
+    let ctx = canvas.getContext('2d');
+
+    ctx.fillRect(300, 300, 100, 100);
+
+}
+
+draw()
+
+
+
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+    document.getElementById("start-button").onclick = function() {
+        startGame();
+    };
 
-  function startGame() {
 
-  }
+
+    function startGame() {
+
+    }
 };
